13ddb2e56489165bc7526f315b10ac47806d99a3
Tyler McAdams <tylermcadams@hushmail.com>
2024-04-03 11:38:26 -0400

    Initial commit

---
+++ test/testcore-cdk-cross-acct.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestcoreCdkCrossAcct from '../lib/testcore-cdk-cross-acct-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/testcore-cdk-cross-acct-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestcoreCdkCrossAcct.TestcoreCdkCrossAcctStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
